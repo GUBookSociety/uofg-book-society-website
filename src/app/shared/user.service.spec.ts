3811e7cc9d24e5427b65dcc73a80ad834440c63f
Liam Lau <2314991l@student.gla.ac.uk>
2020-06-19 21:23:15 +0100

    Adding necessary imports to fix user service test

--- src/app/shared/user.service.spec.ts
+++ src/app/shared/user.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../../environments/environment';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase)
+      ]
+    });
     service = TestBed.inject(UserService);
   });
 
