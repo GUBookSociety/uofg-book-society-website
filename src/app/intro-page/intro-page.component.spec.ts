a65b071cabdf5a8268d3c6f697699dd59e28e730
Liam Lau <2314991l@student.gla.ac.uk>
2020-09-22 15:50:02 +0100

    Added animations for main page

--- src/app/intro-page/intro-page.component.spec.ts
+++ src/app/intro-page/intro-page.component.spec.ts
@@ -19,7 +19,5 @@ describe('IntroPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
--- src/app/intro-page/intro-page.component.ts
+++ src/app/intro-page/intro-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var anime: any;
+
 @Component({
   selector: 'app-intro-page',
   templateUrl: './intro-page.component.html',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPageComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    anime({
+      targets: '#logo, .topNavbar',
+      translateY: [-150, 0],
+      opacity: [0, 1],
+      easing: 'easeInOutQuart',
+      duration: 500,
+      delay: 500
+    });
+  }
+
+  mouseEnter(link){
+    anime({
+      targets: link,
+      translateY: [0, 5],
+      easing: 'easeInOutQuart',
+      duration: 200,
+    });
+  }
+
+  mouseLeave(link){
+    anime({
+      targets: link,
+      translateY: [5, 0],
+      easing: 'easeInOutQuart',
+      duration: 200,
+    });
   }
 
 }
